8c80cfed8e29ce546b0a3192a600cea3f8167555
Ritika <102703358+writetoritika@users.noreply.github.com>
2023-10-27 17:42:35 -0500

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -60,9 +60,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0fc5d935ebf8bc3bc"
+  ami                    = "ami-0e83be366243f524a"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "Mac"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
