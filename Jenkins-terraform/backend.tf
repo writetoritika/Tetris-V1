16d6c59684b8abb8cec59ba435c0a5f2e452b1c7
Ritika <102703358+writetoritika@users.noreply.github.com>
2023-10-27 17:55:56 -0500

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "writetoritika" # Replace with your actual S3 bucket name
+    bucket = "ritika03" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-2"
   }
