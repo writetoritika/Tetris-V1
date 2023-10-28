fe5f2324e769620403ca2a73897f08caf93cdb9c
Ritika <102703358+writetoritika@users.noreply.github.com>
2023-10-27 19:30:00 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "writetoritika" # Replace with your actual S3 bucket name
+    bucket = "ritika030" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
